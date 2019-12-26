89943a2d8d99c0a9a64f87d2fe0e6ca4e1fdc5ce
ys199508 <893534362@qq.com>
2019-12-26 22:27:05 +0800

    配置文件

--- lufei_pc/src/settings.js
+++ lufei_pc/src/settings.js
@@ -1,6 +1,6 @@
 export default {
   // 服务端的api地址
-  Host: "http://api.lxh1.com",
+  Host: "http://api.yangsenjacob.club",
   check_user_login(){
     return localStorage.user_token || sessionStorage.user_token;
   }
